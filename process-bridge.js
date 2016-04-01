615e399c254d355329c2de5ad2b78a80efa66953
anseki <anseki@users.noreply.github.com>
2016-04-01 20:55:58 +0900

    JSDoc

--- process-bridge.js
+++ process-bridge.js
@@ -31,7 +31,7 @@ var
  */
 
 /**
- * Normalize an IPC message, and call the function with `requestId`.
+ * Normalize an IPC message, and call `ProcMessage` callback with `requestId`.
  * @param {Object} message - IPC message.
  * @param {ProcMessage} cb - Callback function that is called.
  * @returns {any} result - Something that was returned by `cb`.
@@ -47,7 +47,7 @@ function parseIpcMessage(message, cb) {
 }
 
 /**
- * Normalize an IPC message from input stream, and call the function with `requestId`.
+ * Normalize an IPC message from input stream, and call `ProcMessage` callback with `requestId`.
  *    Extract those from current input stream that includes chunk data, and return remaining data.
  * @param {string} lines - current input stream.
  * @param {boolean} [getLine] - Get a line as plain string.
